58aedc090bf2b0d82f73f9302a22987dc9f4cbca
erikvaningen <erikvaningen@gmail.com>
2013-11-13 14:58:09 +0100

    renaming to skate

--- skate-search-client/src/test/java/org/skate/client/SkateClientGetTest.java
+++ skate-search-client/src/test/java/org/skate/client/SkateClientGetTest.java
@@ -3,6 +3,7 @@ package org.skate.client;
 import org.jglue.cdiunit.AdditionalClasses;
 import org.jglue.cdiunit.CdiRunner;
 import org.junit.runner.RunWith;
+import org.skate.client.SearchClient;
 import org.skate.service.test.SkateAbstractServiceTest;
 
 /**
---
+++ skate-search-interface-dto/src/main/java/org/skate/service/SkateException.java
@@ -0,0 +1,31 @@
+package org.skate.service;
+
+/**
+ * Generic skate runtime exception. skate so far does not have checked
+ * exceptions, the idea behind is that checked exceptions create a lot of ugly
+ * boilerplate code, and more important, all code should be tested so that
+ * exception will not occur runtime.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Erik van Ingen
+ * 
+ */
+public class SkateException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -728976215271142139L;
+
+	public SkateException(Exception e) {
+		super(e);
+	}
+
+	public SkateException(String message) {
+		super(message);
+	}
+
+}
